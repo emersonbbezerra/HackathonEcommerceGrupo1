5ac788d379fec4ccd72b36cfa6996e0ae44e75e0
Jeferson Santos <jefersonsann@hotmail.com>
2024-08-30 20:56:35 -0300

    fix(routes): Update user route to handle async UserController method

--- backend/src/modules/users/users.routes.ts
+++ backend/src/modules/users/users.routes.ts
@@ -1,9 +1,11 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { UserController } from "./users.controller";
 
 const route = Router();
 const userController = new UserController();
 
-route.post("/user", (req) => userController.create(req.body));
+route.post("/user", async (req: Request, res: Response) =>
+  res.send(await userController.create(req.body)),
+);
 
 export { route as UserRoute };
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,8 @@
-import { routeNotFound } from "@/middleware";
 import { UserRoute } from "@/modules/users/users.routes";
 import { Router } from "express";
 
 const routes = Router();
 
 routes.use("/api", UserRoute);
-routes.use(routeNotFound);
 
 export { routes };
