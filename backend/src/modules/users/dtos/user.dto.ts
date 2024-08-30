4da0f206a59100c03695f75660c559dec25abfb7
Jeferson Santos <jefersonsann@hotmail.com>
2024-08-30 14:30:59 -0300

    reactor: user dto with zod validator

---
+++ backend/src/modules/users/dtos/user.dto.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import { User } from "../entities/user";
+
+const CreateUserDTO = User.pick({
+  first_name: true,
+  last_name: true,
+  email: true,
+  phone: true,
+  password: true,
+})
+  .extend({ confirmPassword: z.string().min(5) })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "confirmPassword do not match password",
+  });
+
+const UpdateUserDTO = User.partial();
+
+const UpdatePasswordUserDTO = z
+  .object({
+    oldPassword: z.string().min(5),
+    newPassword: z.string().min(5),
+    confirmNewPassword: z.string().min(5),
+  })
+  .refine(({ newPassword, confirmNewPassword }) => {
+    if (newPassword !== confirmNewPassword)
+      return { message: "password e confirmação devem ser iguais" };
+  });
+
+export type CreateUserType = z.infer<typeof CreateUserDTO>;
+export type UpdateUserType = z.infer<typeof UpdateUserDTO>;
+export type UpdatePasswordUserType = z.infer<typeof UpdatePasswordUserDTO>;
+
+export { CreateUserDTO, UpdatePasswordUserDTO, UpdateUserDTO };
--- backend/src/modules/users/entities/user.ts
+++ backend/src/modules/users/entities/user.ts
@@ -1,8 +1,13 @@
-export interface User {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-  created_at: Date;
-  updated_at: Date;
-}
+import { z } from "zod";
+
+export const User = z.object({
+  id: z.string().uuid(),
+  first_name: z.string().min(1),
+  last_name: z.string().min(1),
+  email: z.string().email().optional(),
+  image: z.string().url().optional(),
+  phone: z.number().optional(),
+  password: z.string().min(5),
+  created_at: z.string().date(),
+  updated_at: z.string().date(),
+});
