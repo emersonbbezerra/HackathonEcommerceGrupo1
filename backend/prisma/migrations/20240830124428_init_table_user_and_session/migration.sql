f3d2c65642f1b7f883c57fa265936d99f0a4de1a
Jeferson Santos <jefersonsann@hotmail.com>
2024-08-30 14:52:42 -0300

    feat: add prisma schema and prisma client

---
+++ backend/prisma/migrations/20240830124428_init_table_user_and_session/migration.sql
@@ -0,0 +1,44 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'STAFFE', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "image" TEXT,
+    "password" TEXT NOT NULL,
+    "phone" INTEGER NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "token" TEXT,
+    "user_agent" TEXT,
+    "ip" TEXT,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_phone_key" ON "users"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_userId_token_key" ON "sessions"("userId", "token");
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ backend/src/config/database/prisma.ts
@@ -0,0 +1,3 @@
+import { PrismaClient } from "@prisma/client";
+
+export class prisma extends PrismaClient {}
