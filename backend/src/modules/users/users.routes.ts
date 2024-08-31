540a5224dafe2d7acc89d62658ecb598a2d147b8
Jeferson Santos <jefersonsann@hotmail.com>
2024-08-31 09:36:19 -0300

    feat(user-routes): news routes of users

--- backend/src/modules/users/users.routes.ts
+++ backend/src/modules/users/users.routes.ts
@@ -4,8 +4,14 @@ import { UserController } from "./users.controller";
 const route = Router();
 const userController = new UserController();
 
-route.post("/user", async (req: Request, res: Response) =>
+route.get("/users", async (_: Request, res: Response) =>
+  res.send(await userController.findAll()),
+);
+route.post("/users", async (req: Request, res: Response) =>
   res.send(await userController.create(req.body)),
+);
+route.get("/users/:id", async (req: Request, res: Response) =>
+  res.send(await userController.findOne(req.params)),
 );
 
 export { route as UserRoute };
