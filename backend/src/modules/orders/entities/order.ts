41b5b6982622b70def44fec69660729ffaa77e5e
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-08-31 19:08:11 -0300

    feat: :sparkles: add Zod validation schema for Order and OrderItem entities
    - Implemented Zod validation schemas for Order and OrderItem - Added validation for fields including enums, arrays, and nested objects

--- backend/src/modules/orders/entities/order.ts
+++ backend/src/modules/orders/entities/order.ts
@@ -1,25 +1,39 @@
-import { Product } from "@/modules/products/entities/product";
+import { z } from "zod";
 
-export interface Order {
-  orderId: string;
-  customerName: string;
-  customerPhone: string;
-  customerAddress: string;
-  items: OrderItem[];
-  totalPrice: number;
-  orderStatus: "pending" | "preparing" | "delivering" | "delivered";
-  orderDate: Date;
-  paymentMethod:
-    | "cash"
-    | "credit_card"
-    | "debit_card"
-    | "paypal"
-    | "apple_pay"
-    | "google_pay";
-}
+const OrderItem = z.object({
+  product: z.object({
+    id: z.string().uuid(),
+    name: z.string().min(1),
+    description: z.string().min(1),
+    image: z.string().url().nullable(),
+    price: z.number().positive(),
+    category: z.string().min(1),
+    created_at: z.date(),
+    updated_at: z.date(),
+  }),
+  quantity: z.number().positive(),
+  price: z.number().positive(),
+});
 
-export interface OrderItem {
-  product: Product;
-  quantity: number;
-  price: number;
-}
+const Order = z.object({
+  orderId: z.string().uuid(),
+  customerName: z.string().min(1),
+  customerPhone: z.string().min(1),
+  customerAddress: z.string().min(1),
+  items: z.array(OrderItem),
+  totalPrice: z.number().positive(),
+  orderStatus: z.enum(["pending", "preparing", "delivering", "delivered"]),
+  orderDate: z.date(),
+  paymentMethod: z.enum([
+    "cash",
+    "credit_card",
+    "debit_card",
+    "paypal",
+    "apple_pay",
+    "google_pay",
+  ]),
+});
+
+export type OrderSchema = z.infer<typeof Order>;
+
+export { Order, OrderItem };
