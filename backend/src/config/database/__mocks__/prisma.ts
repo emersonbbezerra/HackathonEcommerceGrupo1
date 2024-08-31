50f914b4336bd2dc81e0f960e0c71d6b7d7b5c31
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-08-31 18:35:28 -0300

    fix: :bug: update Prisma mock to include product model

--- backend/src/config/database/__mocks__/prisma.ts
+++ backend/src/config/database/__mocks__/prisma.ts
@@ -10,6 +10,7 @@ export const prismaMock = {
   product: {
     findMany: jest.fn(),
     findFirst: jest.fn(),
+    findUnique: jest.fn(),
     create: jest.fn(),
     update: jest.fn(),
     delete: jest.fn(),
