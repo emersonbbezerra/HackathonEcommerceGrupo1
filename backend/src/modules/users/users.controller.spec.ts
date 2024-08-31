e41ec46ae3c3fc0413a4bae827d9c80611f5abff
Jeferson Santos <jefersonsann@hotmail.com>
2024-08-31 09:07:56 -0300

    test(find-all): implement a find all in controller

--- backend/src/modules/users/users.controller.spec.ts
+++ backend/src/modules/users/users.controller.spec.ts
@@ -2,6 +2,7 @@ import { ServerResponse } from "@/common/constants";
 import { ZodValidateError } from "@/common/errors/zod-validate-error";
 import { prismaMock } from "@/config/database/__mocks__/prisma";
 import { CreateUserType } from "./dtos/user.dto";
+import { UserSchema } from "./entities/user";
 import { IUserController, UserController } from "./users.controller";
 import { UsersService } from "./users.service";
 
@@ -16,6 +17,55 @@ describe("UserController", () => {
       prismaMock,
     ) as jest.Mocked<UsersService>;
     userController = new UserController(usersServiceMock);
+  });
+
+  describe("findAll users", () => {
+    it("Should successfully find all users", async () => {
+      const usersData: UserSchema[] = [
+        {
+          id: "d426bcf6-8536-41f3-91ba-c39c581554e2",
+          first_name: "any_fname",
+          last_name: "any_lname",
+          email: "any@email.com",
+          image: null,
+          password: "hash",
+          phone: 123456789,
+          role: "USER",
+          createdAt: new Date("2024-08-30T19:30:57.510Z"),
+          updatedAt: new Date("2024-08-30T19:30:57.510Z"),
+        },
+        {
+          id: "d426bcf6-8536-41f3-91ba-c39c345678w1",
+          first_name: "other_fname",
+          last_name: "other_lname",
+          email: "other@email.com",
+          image: null,
+          password: "hash",
+          phone: 9876543210,
+          role: "USER",
+          createdAt: new Date("2024-08-28T19:30:57.510Z"),
+          updatedAt: new Date("2024-08-28T19:30:57.510Z"),
+        },
+      ];
+      usersServiceMock.findAll.mockResolvedValueOnce(usersData);
+
+      const resp = await userController.findAll();
+
+      expect(resp).toBeInstanceOf(ServerResponse);
+      expect(resp).toEqual(
+        new ServerResponse(200, "Successfully find all user", usersData),
+      );
+    });
+
+    it("Should return ServerResponse with error when findAll fails", async () => {
+      const errorMessage = "Failed to find users";
+      usersServiceMock.findAll.mockRejectedValueOnce(new Error(errorMessage));
+
+      const resp = await userController.findAll();
+
+      expect(resp).toBeInstanceOf(ServerResponse);
+      expect(resp).toEqual(new ServerResponse(400, errorMessage));
+    });
   });
 
   describe("Create user", () => {
@@ -48,7 +98,7 @@ describe("UserController", () => {
       );
     });
 
-    it("Should return ServerError when user creation fails", async () => {
+    it("Should return ServerResponse when user creation fails", async () => {
       usersServiceMock.create.mockRejectedValueOnce(
         new Error("Creation failed"),
       );
