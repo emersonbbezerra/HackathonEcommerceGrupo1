cacf051f0de5675f95c65333889c1b1ba0285887
Jeferson Santos <jefersonsann@hotmail.com>
2024-09-01 15:07:24 -0300

    fix: add middleware authentication in logout

--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -1,9 +1,9 @@
+import {
+  AuthenticatedRequest,
+  authMiddleware,
+} from "@/middleware/auth.middleware";
 import { Request, Response, Router } from "express";
 import { AuthController } from "./auth.controller";
-
-interface AuthenticatedRequest extends Request {
-  user?: any;
-}
 
 const route = Router();
 const authController = new AuthController();
@@ -11,8 +11,11 @@ const authController = new AuthController();
 route.post("/login", async (req: Request, res: Response) =>
   res.send(await authController.login(req.body)),
 );
-route.post("/logout", async (req: AuthenticatedRequest, res: Response) =>
-  res.send(await authController.logout(req.user.userId)),
+route.post(
+  "/logout",
+  authMiddleware,
+  async (req: AuthenticatedRequest, res: Response) =>
+    res.send(await authController.logout(req.user.userId)),
 );
 
 export { route as AuthRoutes };
