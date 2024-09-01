b3e049f502bbabf16ad5ea34a90c1a85689a53a5
Jeferson Santos <jefersonsann@hotmail.com>
2024-09-01 15:09:32 -0300

    fix: add middleware authentication in route users

--- backend/src/modules/users/users.routes.ts
+++ backend/src/modules/users/users.routes.ts
@@ -1,3 +1,7 @@
+import {
+  AuthenticatedRequest,
+  authMiddleware,
+} from "@/middleware/auth.middleware";
 import { Request, Response, Router } from "express";
 import { UserController } from "./users.controller";
 
@@ -13,11 +17,18 @@ route.post("", async (req: Request, res: Response) =>
 route.get(":id", async (req: Request, res: Response) =>
   res.send(await userController.findOne(req.params)),
 );
-route.patch(":id", async (req: Request, res: Response) => {
-  res.send(await userController.update({ id: req.params.id, body: req.body }));
-});
-route.delete(":id", async (req: Request, res: Response) =>
-  res.send(await userController.delete(req.params.id)),
+route.patch(
+  "",
+  authMiddleware,
+  async (req: AuthenticatedRequest, res: Response) => {
+    res.send(await userController.update({ id: req.user.id, body: req.body }));
+  },
+);
+route.delete(
+  "",
+  authMiddleware,
+  async (req: AuthenticatedRequest, res: Response) =>
+    res.send(await userController.delete(req.user.id)),
 );
 
 export { route as UserRoute };
