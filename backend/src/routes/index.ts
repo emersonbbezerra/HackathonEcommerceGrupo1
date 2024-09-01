ae61312109b159462118a39503acd903afff6f39
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-09-01 12:32:23 -0300

    refactor: update main router to include orders routes without duplication
    - Added `OrderRoute` to the main router.

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,12 @@
 import { UserRoute } from "@/modules/users/users.routes";
+import { ProductRoute } from "@/modules/products/products.routes";
+import { OrderRoute } from "@/modules/orders/orders.routes";
 import { Router } from "express";
 
 const routes = Router();
 
 routes.use("/api", UserRoute);
+routes.use("/api", ProductRoute);
+routes.use("/api", OrderRoute);
 
 export { routes };
