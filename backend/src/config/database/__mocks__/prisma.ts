e1260632d2bc506b86ec9b1ad5073ca30535b457
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-08-31 21:39:09 -0300

    feat: :sparkles: add Order and OrderItem models to prismaMock with necessary methods
    - Added `order` model to `prismaMock` with methods: findMany, findFirst, findUnique, create, update, delete. - Added `orderItem` model to `prismaMock` with methods: create, updateMany.

--- backend/src/config/database/__mocks__/prisma.ts
+++ backend/src/config/database/__mocks__/prisma.ts
@@ -15,4 +15,16 @@ export const prismaMock = {
     update: jest.fn(),
     delete: jest.fn(),
   },
+  order: {
+    findMany: jest.fn(),
+    findFirst: jest.fn(),
+    findUnique: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  },
+  orderItem: {
+    create: jest.fn(),
+    updateMany: jest.fn(),
+  },
 };
