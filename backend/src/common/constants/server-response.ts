777f34cc0e0cfefed0a712814d0713258ff89f6f
Jeferson Santos <jefersonsann@hotmail.com>
2024-09-06 15:28:44 -0300

    refactor: custom ServerResponse return data no more respData

--- backend/src/common/constants/server-response.ts
+++ backend/src/common/constants/server-response.ts
@@ -1,21 +1,21 @@
 export class ServerResponse<T> {
   readonly message: string;
   private readonly statusCode: number;
-  private readonly respData?: T;
+  private readonly data?: T;
 
   constructor(statusCode: number, message: string, data?: T) {
     this.statusCode = statusCode;
     this.message = message;
-    this.respData = data;
+    this.data = data;
   }
 
-  public get data(): { statusCode: number; message: string; data?: T } {
+  public get response(): { statusCode: number; message: string; data?: T } {
     const resp = {
       statusCode: this.statusCode,
       message: this.message,
-      data: this.respData,
+      data: this.data,
     };
-    if (!resp?.data) delete resp.data;
+    if (resp.data === undefined) delete resp.data;
     return resp;
   }
 }
