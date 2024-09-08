5287f0cf12b771353bce671b8e1ba1e9952e2b60
Jeferson Santos <jefersonsann@hotmail.com>
2024-09-07 21:13:47 -0300

    fix: string image validation zod

--- backend/src/modules/products/entities/product.ts
+++ backend/src/modules/products/entities/product.ts
@@ -4,7 +4,7 @@ const Product = z.object({
   id: z.string().uuid(),
   name: z.string().min(1),
   description: z.string().min(1),
-  image: z.string().url().nullable(),
+  image: z.string().nullable(),
   price: z.number().positive(),
   category: z.string().min(1),
   created_at: z.date(),
